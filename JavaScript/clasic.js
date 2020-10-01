773dd67d52ae66c7923278fb288011cb0ef3858b
Kosasih Wahid Pramana <sultankosasih@gmail.com>
2020-10-02 02:18:55 +0700

    add author and description

--- JavaScript/clasic.js
+++ JavaScript/clasic.js
@@ -1,3 +1,9 @@
+// [SHORT DESCRIPTION HERE]
+// simplest way to print FizzBuzz with javascript
+// Author: @coz-git
+
+// ...
+
 for (i = 0; i < 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         document.write('FizzBuzz', '&nbsp;')
