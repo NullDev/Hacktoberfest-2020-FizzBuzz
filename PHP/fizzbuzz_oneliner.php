bfcf4438607bc33ae8bcf56918e8b3ccf5febbae
Scarwolf <Scarwolf@users.noreply.github.com>
2020-10-02 00:06:55 +0200

    Update fizzbuzz_oneliner.php

--- PHP/fizzbuzz_oneliner.php
+++ PHP/fizzbuzz_oneliner.php
@@ -3,4 +3,4 @@
 // FizzBuzz One liner Using PHP 7.4 Arrow functions & nested ternary operators
 // Author: @Scarwolf
 
-foreach (array_map(fn($v) => !($v % 15) ? "FizzBuzz" : (!($v % 5) ? "Buzz" : (!($v % 3) ? "Fizz" : $v)), range(1, 100)) as $value) { echo $value . '<br>'; }
+echo implode('<br>', array_map(fn($v) => (!($v % 3) && !($v % 5)) ? "FizzBuzz" : (!($v % 5) ? "Buzz" : (!($v % 3) ? "Fizz" : $v)), range(1, 100)));
