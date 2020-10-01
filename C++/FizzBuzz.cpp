4637405a14625c53e4a88ad9318eefd60a0c70ab
Divyanshu <kumardivy1999@gmail.com>
2020-10-01 07:24:13 +0530

    c++ solution of fizzbuzz

---
+++ C++/FizzBuzz.cpp
@@ -0,0 +1,19 @@
+// This is C++ solution of FizzBuzz 
+// author: @divyanshugit
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=100;i++){
+        if((i%3 == 0) && (i%5==0))
+            cout<<"FizzBuzz\n";
+        else if(i%3 == 0)
+            cout<<"Fizz\n";
+        else if(i%5 == 0)
+            cout<<"Buzz\n";
+        else
+            cout<<i<<"\n";
+     }
+    return 0;
+}
