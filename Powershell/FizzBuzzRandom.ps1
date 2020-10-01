061cead01682c57ccb4a6fd8a0f6357a425949fc
De1337NY <shepm09@gmail.com>
2020-10-01 14:54:41 -0400

    Add FizzBuzz Powershell with Random Numbers picked

---
+++ Powershell/FizzBuzzRandom.ps1
@@ -0,0 +1,13 @@
+﻿for ($i=0; $i -le 100; $i++){
+$r = Get-Random -Maximum 100
+if (($r%3 -eq 0) -and ($r%5 -eq 0)){
+write-output "FizzBuzz ($r)"
+} elseif($r%3 -eq 0){
+Write-Output "Fizz ($r)"
+}elseif ($r%5 -eq 0){
+Write-Output "Buzz ($r)"
+}else{
+Write-Output $r
+}
+
+}
