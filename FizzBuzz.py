a99737162434398ce6d0e0be6285c2fee056d44a
cyberGhoulK <71748495+cyberGhoulK@users.noreply.github.com>
2020-10-01 13:32:41 +0530

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+#A simpler Python code to implement FizzBuzz
+#Author @cyberGhoulK
+
+for num in range(1, 101):
+  st = ''
+  if num%3 == 0:
+    st += 'Fizz'
+  if num%5 == 0:
+    st += 'Buzz'
+  print(st) if st else print(i)
+    
