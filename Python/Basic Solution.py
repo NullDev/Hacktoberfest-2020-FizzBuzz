e222d6ab0abf9151dd71d42a17735b2e53deb460
Sombit Ghosh <sombit0503@gmail.com>
2020-10-01 13:59:10 +0530

    Basic Solution

---
+++ Python/Basic Solution.py
@@ -0,0 +1,13 @@
+// Fizzzzz.......Buzzzzzzzzzz
+//Author: @BreathlessVictor on GitHub
+//email: sombit0503@gmail.com
+
+for i in range (101):
+    if i % 3 == 0:
+        print ("Fizz")
+    elif i % 5 == 0:
+        print ("Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print ("FizzBuzz")
+    else:
+         print(i)
