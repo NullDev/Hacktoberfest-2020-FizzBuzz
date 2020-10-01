60bc83cdb0b818012cd6c57ba48676d8fbb52e79
Suvarnesh K M <suvarnesh1729@gmail.com>
2020-10-01 18:15:47 +0530

    added Simple-FizzBuzz.py

---
+++ Python/Simple-FizzBuzz.py
@@ -0,0 +1,14 @@
+#A simple Python code to implement FizzBuzz
+#Author @SuvarneshKM
+
+for fizzbuzz in range(1,101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("FizzBuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("Fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("Buzz")
+        continue
+    print(fizzbuzz)
