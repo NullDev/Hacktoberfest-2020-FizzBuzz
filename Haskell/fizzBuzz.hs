8ab9ebc89a64652711a282332bbd8d712b60bef6
Mainak Debnath <debnath.mainak07@gmail.com>
2020-10-01 18:14:11 +0530

    Created fizzbuzz in haskell

---
+++ Haskell/fizzBuzz.hs
@@ -0,0 +1,11 @@
+module Main where
+main :: IO ()
+main = printAll $ map fizzBuzz [1..100]
+ where
+ printAll [] = return ()
+ printAll (x:xs) = putStrLn x >> printAll xs
+fizzBuzz :: Integer -> String
+fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
+ | n `mod` 5 == 0 = "Fizz"
+ | n `mod` 3 == 0 = "Buzz"
+ | otherwise = show n
