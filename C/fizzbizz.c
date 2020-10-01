c1dec44f395a77401b3763f5e6d40cff54354b93
Darshan Jain <46564737+darshanjain2000@users.noreply.github.com>
2020-10-01 12:31:36 +0530

    FizzBuzz

--- C/fizzbizz.c
+++ C/fizzbizz.c
@@ -1,15 +1,15 @@
 //This code simply going into loop and printing values but checking
-//if any no is multiple of 3,5,both(15) and prints values accordingly
+//if any no is multiple of 3,5,both(15 or can do it individually) and prints values accordingly
 //Author: @darshanjain2000
 
 #include <stdio.h> 
   
-int main(void) 
+int main() 
 { 
     int i; 
     for (i=1; i<=100; i++) 
     { 
-        if (i%15 == 0)         
+        if (i%3==0 and i%5 == 0)         
             printf ("FizzBuzz\t");     
         else if ((i%3) == 0)     
             printf("Fizz\t");                  
