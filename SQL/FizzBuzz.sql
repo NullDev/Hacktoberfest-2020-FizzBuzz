17ea8aa3b39a6b822fa8640b21b100471aae8a54
Gaurav Poosarla <poosarlagaurav@gmail.com>
2020-10-02 01:19:51 +0530

    AddedSQL

---
+++ SQL/FizzBuzz.sql
@@ -0,0 +1,18 @@
+with recursive numbers (i) as (
+   select *
+   from (values (1)) as t
+   union all
+   
+   select n.i + 1
+   from numbers n
+   where n.i < 100
+)
+select i,
+       case 
+          when mod(i, 15) = 0 then 'Fizz Buzz'
+          when mod(i,  5) = 0 then 'Buzz'
+          when mod(i,  3) = 0 then 'Fizz'
+          else cast(i as varchar)
+       end as fz
+from numbers 
+order by i;
