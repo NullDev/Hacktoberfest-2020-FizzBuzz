7c11d3921e8fe5aaf904c61eb0b4a8dba1a79113
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-10-01 21:01:45 +0530

    Created if-else statement to print words

---
+++ C/FizzBuzz-if.c
@@ -0,0 +1,18 @@
+//FizzBuzz in C
+//Author: Chamodi Thennakoon(chamodit)
+
+#include<stdio.h>
+int main(void)
+{
+    int x;
+
+    if(x == 1){
+        printf("Fizz");
+    }else if(x == 2){
+        printf("Buzz");
+    }else{
+        printf("FizzBuzz");
+    }
+
+    return 0;
+}
