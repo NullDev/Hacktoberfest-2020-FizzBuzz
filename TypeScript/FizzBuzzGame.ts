1988a5e0271c9dcecc2e9a622e17a7c559c15993
Sheetal Dadhich <56470040+Sheetal575@users.noreply.github.com>
2020-10-01 22:41:39 +0530

    1 to 100 number print some character on dividing with specific number.
    A for loop will run for 1 to 100 number  then it will check if the number is divded by 3 , 5 or both 3 and 5 then print "FIzz" , "Buzz" or "FizzBuzz" respectively.

---
+++ TypeScript/FizzBuzzGame.ts
@@ -0,0 +1,21 @@
+class fizzbuzzGame{
+    fizzbuzzNumber:number=0
+    fizzbuzz(){
+        // a forloop which run for the number from 1 to 100
+        for(this.fizzbuzzNumber=1;this.fizzbuzzNumber<100;this.fizzbuzzNumber++){
+          if(this.fizzbuzzNumber%3==0 ){
+              //if the number is divided by 3 then it prints "Fizz"
+            console.log("fizz")
+          }else if(this.fizzbuzzNumber%5==0){
+              //if the number is divided by 3 then it prints "buzz"
+            console.log("Buzz")
+          }else if(this.fizzbuzzNumber%3==0 && this.fizzbuzzNumber%5==0){
+              //if the number is divided by 3 and also 5 then it prints "buzz"
+            console.log("FizzBuzz")
+          }
+          else{
+            console.log(this.fizzbuzzNumber)
+          } 
+        }
+      }
+}
