7433c0d1b255ca638d014ee722a36dbcb0c00c79
Ankit Chawla <ankitc226@gmail.com>
2020-10-01 08:20:39 +0530

    Fizzbuzz in C++

---
+++ C++/normal_fizzbuzz.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	for(int i=0;i<=100;i++)
+	{
+		if(i%3 == 0 && i%5 == 0)
+			cout<<"fizzbuzz";
+		else
+		if(i%5 == 0)
+			cout<<"buzz";
+		else
+		if(i%3 == 0)
+			cout<<"fizz";
+		else
+			cout<<i;
+		cout<<"\n";
+	}
+}
