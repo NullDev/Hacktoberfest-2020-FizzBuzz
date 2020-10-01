33823c7a03babf95d4d08be0b1234a6282933dbc
Anton Orlov <ion6431@gmail.com>
2020-10-02 02:30:50 +0300

    Simple Elm FizzBuzz

---
+++ Elm/FizzBuzzSimple.elm
@@ -0,0 +1,16 @@
+import Html exposing (text)
+
+
+main =
+  text (Debug.toString (List.map textToShow (List.range 1 100)))
+  
+textToShow number = 
+  if (remainderBy 15)      number == 0 then
+    "FizzBuzz"
+  else if (remainderBy 3)  number == 0 then
+    "Fizz"
+  else if (remainderBy 5)  number == 0 then
+    "Buzz"
+  else
+    String.fromInt number
+  
