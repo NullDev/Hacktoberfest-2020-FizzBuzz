9e97a4c35c015b0633fddb9b95014407a1094e93
ZachyDev <mosetizachary001@gmail.com>
2020-10-01 12:24:27 +0300

    finished my FizzBuzz Solution : JavaScript✅✅

--- JavaScript/one-liner.js
+++
@@ -1,5 +0,0 @@
-// A neat JavaScript one-liner to solve FizzBuzz 
-// (from my gist: https://gist.github.com/NLDev/d620a1808fc445afe79875690db78da3)
-// Author: @NLDev
-
-Array.apply([], Array(100)).map((e, i) => [["fizz"][i % 3], ["buzz"][i % 5]].join("") || i).forEach(i => console.log(i));
