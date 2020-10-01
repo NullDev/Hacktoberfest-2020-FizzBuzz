3c8420863aa352c11dfb1c6a3a53b2ab1abe802a
Nidhi Gupta <nigupta519@gmail.com>
2020-10-01 20:50:19 +0530

    Updated fizzbuzzn.c

--- C/fizzbuzzn.c
+++ C/fizzbuzzn.c
@@ -11,24 +11,7 @@ int main()
 {
     for (int i=1; i<=100; i++)
     {
-        if(i % 3 == 0 && i % 5== 0)
-        {
-            printf("FizzBuzz\n");
-        }
-        
-        else if(i % 3 == 0)
-        {
-            printf("Fizz\n");
-        }
-
-        else if(i % 5 == 0)
-        {
-            printf("Buzz\n");
-        }
-        else
-        {
-            printf("%d\n",i);
-        }
+        (i % 3 == 0 && i % 5== 0) ? printf("FizzBuzz\n") : (i % 3 == 0) ? printf("Fizz\n") :( (i % 5 == 0) ? printf("Buzz\n") : printf("%d\n",i) );
     }
 
     return 0;
