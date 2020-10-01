df1cde52b28b9618507d22c98826572c9a1b922d
cyberGhoulK <71748495+cyberGhoulK@users.noreply.github.com>
2020-10-01 15:35:14 +0530

    Create FizzBuzz - without modulo.py
    This a FizzBuzz program which has a different approach. It is short but doesn't use the modulo (%) operator.

---
+++ Python/FizzBuzz - without modulo.py
@@ -0,0 +1,14 @@
+# An approach to solve FizzBuzz without using modulo operator
+# @cyberGhoulK
+
+def if_divisible_by_3(num: int):
+    num = str(num)
+    while len(num) != 1:
+        s = sum(map(int, num))
+        num = str(s)
+    return int(num) in [3, 6, 9]
+
+def if_divisible_by_5(num: int):
+    return int(str(num)[-1]) in [0, 5]
+
+print("\n".join(map(str, [('Fizz'*(if_divisible_by_3(num)) + 'Buzz'*(if_divisible_by_5(num)) or num) for num in range(1, 101)])))
