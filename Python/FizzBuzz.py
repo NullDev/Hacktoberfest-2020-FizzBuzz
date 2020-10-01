4182c72e37148259dd102923881036332042cae2
Pranay Reddy <a.pranay2001@gmail.com>
2020-10-01 05:57:45 +0530

    Added Python Implementation

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,14 @@
+#A simple Python code to implement FizzBuzz
+#Author @pranay-ar
+
+for fizzbuzz in range(101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
