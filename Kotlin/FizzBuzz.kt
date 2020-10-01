d5fbb5dda58a731307ef74c5d053703260d8c409
Mohit Deshpande <mohitdeshpande@icloud.com>
2020-10-01 10:06:14 -0400

    assign logic to variables to make it more readable

--- Kotlin/FizzBuzz.kt
+++ Kotlin/FizzBuzz.kt
@@ -1,9 +1,11 @@
 fun main() {
   for (i in 1..100) {
+    val fizz = i % 3 == 0
+    val buzz = i % 5 == 0
     when {
-      i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
-      i % 3 == 0 -> println("Fizz")
-      i % 5 == 0 -> println("Buzz")
+      fizz && buzz -> println("FizzBuzz")
+      fizz -> println("Fizz")
+      buzz -> println("Buzz")
       else -> println(i)
     }
   }
