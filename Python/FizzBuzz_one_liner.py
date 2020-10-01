28ac9899b8f98223daeb71aaaa2da8d39d1ec0fd
AMx101010 <59364507+galahad42@users.noreply.github.com>
2020-10-01 18:51:48 +0530

    Update FizzBuzz_one_liner.py
    Updated the initial info as per the README.MD.

--- Python/FizzBuzz_one_liner.py
+++ Python/FizzBuzz_one_liner.py
@@ -1,1 +1,5 @@
-for i in range(1,101):  print("Fizz"*(i%3 == 0) + (i%5 == 0)*"Buzz" or i)
+# My attempt on one liner python script for FizzBuzz solution
+# Author: @galahad42
+
+for i in range(1,101):
+    print("Fizz"*(i%3 == 0) + (i%5 == 0)*"Buzz" or i)
