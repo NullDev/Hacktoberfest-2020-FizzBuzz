37ec4645a6ddf27a4a50a690554a642e85f999ef
amit singh <57305134+amitsingh6391@users.noreply.github.com>
2020-10-01 12:06:29 +0530

    Update Normal-FizzBuzz.py
    i add some comment how we can achive the same thing with diffrent approche

--- Python/Normal-FizzBuzz.py
+++ Python/Normal-FizzBuzz.py
@@ -1,7 +1,9 @@
 #A simple Python code to implement FizzBuzz
 #Author @pranay-ar
 
-for fizzbuzz in range(101):
+# this for loop is run in range 0 to 100 because we know for loops is allways run from 0 not 1 and end with the  one digit befor 
+
+for fizzbuzz in range(101):  ## for fizzbuzz in range(0,101):
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("fizzbuzz")
         continue
