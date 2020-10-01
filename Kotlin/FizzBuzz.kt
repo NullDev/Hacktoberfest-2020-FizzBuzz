c3da09b4b65ca962d81dedcb9d8a7b55732d4db2
Mohit Deshpande <mohitdeshpande@icloud.com>
2020-10-01 09:57:13 -0400

    Simple FizzBuzz

--- Kotlin/FizzBuzz.kt
+++ Kotlin/FizzBuzz.kt
@@ -1,1 +1,10 @@
-
+fun main() {
+  for (i in 1..100) {
+    when {
+      i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
+      i % 3 == 0 -> println("Fizz")
+      i % 5 == 0 -> println("Buzz")
+      else -> println(i)
+    }
+  }
+}
