db4b0a6b1a4a1d9fba87919374739cdb7403bb55
Braintelligence <Braintelligence@users.noreply.github.com>
2020-10-01 18:11:13 +0200

    Remove redundant comment

--- JavaScript/array-index.js
+++ JavaScript/array-index.js
@@ -1,8 +1,6 @@
 // elegant way using array index
 // Author: @hwennnn
 
-// The code actually did explain everything.
-
 function FizzBuzz() {
     for (i = 0; ++i < 101;) {
         console.log([i, "Fizz", "Buzz", "FizzBuzz"][!(i % 3) + !(i % 5) * 2])
