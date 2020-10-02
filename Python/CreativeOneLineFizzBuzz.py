6834ab3ee7590f4c6e73d009d99255b08d519645
Gourav Jain <gourav22859@gmail.com>
2020-10-02 18:23:29 +0530

    Update CreativeOneLineFizzBuzz.py

--- Python/CreativeOneLineFizzBuzz.py
+++ Python/CreativeOneLineFizzBuzz.py
@@ -1,5 +1,5 @@
 # I tried making a simple and creative FizzBuzz function just one line
-# Author: @000Nobody
+# Author: @GJ007
 
 def fizzBuzz(n: int) -> list:
     return(['FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i for i in range(1, n+1)])
