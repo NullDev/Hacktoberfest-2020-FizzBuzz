fb2cd5722b4a5a9ecda16e1ad77e99324bc1a615
Rizki Maulana <kikirizmau140175@gmail.com>
2020-10-02 21:14:24 +0700

    Update fizzbuzz_string_concatenation.dart

--- Dart/fizzbuzz_string_concatenation.dart
+++ Dart/fizzbuzz_string_concatenation.dart
@@ -2,21 +2,21 @@
 // Author: @rmRizki
 
 void main() {
-  int i = 1;
   var output = '';
-  while (i <= 100) {
+  
+  for (int i = 1; i <= 100; i++) {
     if ((i % 3 == 0) || (i % 5 == 0)) {
       if (i % 3 == 0) {
         output += "Fizz" ;
       }
       if (i % 5 == 0) {
         output += "Buzz";
-      };
+      }
     } else {
       output += i.toString();
     }
     output += '\n';
-    i++;
   }
+  
   print(output);
 }
