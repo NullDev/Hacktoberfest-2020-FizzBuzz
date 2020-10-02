cb67e8fe024a17faa19598cc68cf0385db954628
Bitsy-Chuck <singhtejasv9@gmail.com>
2020-10-02 15:02:27 +0530

    Improved readability and added description

--- Java/fizzbuzz-java7.java
+++ Java/fizzbuzz-java7.java
@@ -1,4 +1,19 @@
 //fizzbuzz java programming
 // Author: @Chamudi
 
-public class Solution{public static void main(String[]args){for(int i = 1; i<=100; i++){if(i % 3 == 0)System.out.print("Fizz");if(i%5==0)System.out.print("Buzz");else if(i%3 != 0)System.out.print(i);System.out.println();}}}
+public class Solution{
+  /**
+  NOTE: This has redundancy as (i%3==0) is repeated twice. TO improve it, there are better Solutions
+  */
+  public static void main(String[]args){
+    for(int i = 1; i<=100; i++){
+      if(i%3==0)//if i is divisble by 3
+        System.out.print("Fizz");
+      if(i%5==0)//if i is divisble by 5
+        System.out.print("Buzz");
+      else if(i%3 != 0)//if not divisble by 3 or 5
+        System.out.print(i);
+      System.out.println();
+    }
+  }
+}
