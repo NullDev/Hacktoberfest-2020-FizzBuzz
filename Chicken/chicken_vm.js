ecef17e0e71aa6e837ff6acb79a006a41ed6d506
Chris Witteveen <chriswitteveen2@hotmail.com>
2020-10-03 00:41:49 +0200

    Forgot to add the original chicken function

--- Chicken/chicken_vm.js
+++ Chicken/chicken_vm.js
@@ -21,6 +21,61 @@
  */
 
 
+/**
+ * The original chicken implementation
+ * You can use this the same as the `runChicken` function
+ */
+function chicken(CHICKEN, Chicken) {
+    Chicken &&( chicken. chicken =[,
+        CHICKEN, CHICKEN = Chicken = chicken.
+            $Chicken =-( CHICKEN ==( chicken.
+            Chicken = Chicken ))], chicken.
+        chicken [Chicken++] = chicken. chicken, chicken.
+        CHICKEN = ++Chicken, chicken (--Chicken), chicken.
+        $Chicken = ++Chicken, chicken. CHICKEN++ );
+    Chicken = chicken. Chicken [chicken.
+        $Chicken++ ]; chicken. Chicken = CHICKEN? Chicken?
+        '\012'== Chicken? chicken (++ CHICKEN, chicken.
+            chicken [++ chicken. CHICKEN ]=
+            CHICKEN - CHICKEN ): Chicken
+        ==' '|'\015'== Chicken ||
+        (Chicken   )== "c" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "h" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "i" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "c" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "k" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "e" &  chicken. Chicken [chicken.
+            $Chicken++ ]== "n"&&++chicken. chicken [chicken.
+            CHICKEN]? chicken (CHICKEN)
+            :[ "Error on line "+CHICKEN+": expected 'chicken'",
+                chicken. CHICKEN = CHICKEN ++- CHICKEN ]:
+        chicken. chicken :( CHICKEN = chicken.
+        Chicken[chicken.CHICKEN], Chicken? (Chicken =
+
+        --Chicken? --Chicken? --Chicken? --Chicken? --Chicken?
+            --Chicken? --Chicken? --Chicken? --Chicken?
+                chicken. CHICKEN++ &&
+                --Chicken :'&#'+CHICKEN+';': chicken.
+                    Chicken [chicken. Chicken [-- chicken. CHICKEN ]&&
+                (chicken. $Chicken += CHICKEN), --chicken.
+                    CHICKEN ]: chicken. Chicken [chicken.
+                    Chicken [CHICKEN] = chicken. Chicken
+                    [-- chicken. CHICKEN ],-- chicken. CHICKEN ]:
+                chicken. Chicken [chicken. Chicken [chicken.
+                    $Chicken++ ]] [CHICKEN]: CHICKEN == chicken.
+                Chicken [-- chicken. CHICKEN ]:
+            CHICKEN*chicken. Chicken [-- chicken.
+                CHICKEN ]: chicken. Chicken [-- chicken.
+            CHICKEN ]- CHICKEN: chicken. Chicken [-- chicken.
+            CHICKEN ]+ CHICKEN: chicken.
+            CHICKEN ++ && "chicken", chicken.
+        Chicken [chicken. CHICKEN ]= Chicken, chicken
+    ()): CHICKEN );
+
+    return chicken.
+        Chicken
+}
+
 const REGISTERS = {
     SELF: 0,
     INPUT: 1,
