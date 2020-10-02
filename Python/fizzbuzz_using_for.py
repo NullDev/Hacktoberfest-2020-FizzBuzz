ad291b44ffa450ea9f183fc0f1e9aab9ffe4ca33
singhankush01 <44083904+singhankush01@users.noreply.github.com>
2020-10-03 02:30:45 +0530

    a simple and clean version

---
+++ Python/fizzbuzz_using_for.py
@@ -0,0 +1,12 @@
+''' here is a simple and clean code for fizzbuzz
+  Author: @singhankush01'''
+
+for i in range(1,101):
+  if i%15 == 0:
+    print('fizzbuzz')
+  elif i%3==0:
+    print('fizz')
+  elif i%5==0:
+    print('buzz')
+  else:
+    print(i)
