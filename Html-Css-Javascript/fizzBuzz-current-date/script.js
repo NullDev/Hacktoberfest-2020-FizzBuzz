cc10be1e56ca15d791163a0ff2554c2e15233e96
parhan <apriliandi642@gmail.com>
2020-10-03 03:40:13 +0700

    add new project folder

---
+++ Html-Css-Javascript/fizzBuzz-current-date/script.js
@@ -0,0 +1,34 @@
+function startTime() {
+   const now = new Date();
+   let hour = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+   let minute = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+   let second = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
+
+   const time = `${hour} : ${minute} : ${second}`;
+   const fizzBuzz = `${fizzBuzzCheck(now.getHours())} : ${fizzBuzzCheck(now.getMinutes())} : ${fizzBuzzCheck(now.getSeconds())}`;
+
+   document.getElementById("clock").innerHTML = time;
+   document.getElementById("fizzBuzz").innerHTML = fizzBuzz;
+
+   setTimeout(startTime, 1000);
+}
+
+startTime();
+
+function fizzBuzzCheck(number) {
+   if (number === 0) {
+      return "Nope";
+
+   } else if (number % 15 === 0) {
+      return "FizzBuzz";
+
+   } else if (number % 3 === 0) {
+      return "Fizz";
+
+   } else if (number % 5 === 0) {
+      return "Buzz";
+
+   } else {
+      return "Nope";
+   }
+}
