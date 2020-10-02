8c16a5aa196ffa98b37045530e6d5c20c16cc5ad
Gaurav Poosarla <poosarlagaurav@gmail.com>
2020-10-02 22:44:08 +0530

    Added new elixir code

--- Elixir/FizzBuzz.ex
+++ Elixir/FizzBuzz.ex
@@ -1,12 +1,37 @@
 defmodule FizzBuzz do
-  def fizz do
-    (1..100)
-    |> Enum.map(&buzz/1)
-    |> Enum.each(&IO.puts/1)
-  end
+	def perform_count(total_count) when total_count >= 0 do
+		total_count
+		|> count()
+		|> Enum.reverse()
+		|> print()
+	end
+	
+	defp count(0) do
+		[]
+	end
+	
+	defp count(count) when rem(count, 3) == 0 and rem(count, 5) == 0 do
+		["fizzbuzz" | count(count - 1)]
+	end
+	
+	defp count(count) when rem(count, 3) == 0 do
+		["fizz" | count(count - 1)]
+	end
+	
+	defp count(count) when rem(count, 5) == 0 do
+		["buzz" | count(count - 1)]
+	end
+	
+	defp count(count) do
+		[Integer.to_string(count) | count(count - 1)]
+	end
 
-  defp buzz(i) when rem(i, 15) == 0, do: "FizzBuzz"
-  defp buzz(i) when rem(i, 5)  == 0, do: "Buzz"
-  defp buzz(i) when rem(i, 3)  == 0, do: "Fizz"
-  defp buzz(i),                      do: Integer.to_string(i)
+	defp print([]) do
+	end
+	
+	defp print([head | tail]) do
+		IO.puts(head)
+		
+		print(tail)
+	end	
 end
