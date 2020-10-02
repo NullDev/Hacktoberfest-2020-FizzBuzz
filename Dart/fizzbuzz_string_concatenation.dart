ad2c3108b9d5292559d3215dcaed9d1cc07e4c47
Rizki Maulana <kikirizmau140175@gmail.com>
2020-10-02 21:11:10 +0700

    Create fizzbuzz_string_concatenation.dart

---
+++ Dart/fizzbuzz_string_concatenation.dart
@@ -0,0 +1,22 @@
+// FizzBuzz with string concatenation
+// Author: @rmRizki
+
+void main() {
+  int i = 1;
+  var output = '';
+  while (i <= 100) {
+    if ((i % 3 == 0) || (i % 5 == 0)) {
+      if (i % 3 == 0) {
+        output += "Fizz" ;
+      }
+      if (i % 5 == 0) {
+        output += "Buzz";
+      };
+    } else {
+      output += i.toString();
+    }
+    output += '\n';
+    i++;
+  }
+  print(output);
+}
