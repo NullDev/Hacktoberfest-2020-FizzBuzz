9d2d234930f00fda4bb3739aa2ced91f5a316fcd
karn21 <karn212000@gmail.com>
2020-10-02 08:29:41 +0530

    add recursive fizzbuzz python

---
+++ Python/recursive-fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizzbuzz(n):
+	if n == 101:
+		return
+	if n%3 == 0 and n%5 == 0:
+		print("fizzbuzz")
+		return fizzbuzz(n+1)
+	if n%3 == 0:
+		print("Fizz")
+		return fizzbuzz(n+1)
+	if n%5 == 0:
+		print("Buzz")
+		return fizzbuzz(n+1)
+	print(n)
+	return fizzbuzz(n+1)
+
+fizzbuzz(1)
