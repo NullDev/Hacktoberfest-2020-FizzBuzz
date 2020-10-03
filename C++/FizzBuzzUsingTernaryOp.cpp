c4645b52d2c3be9ed8ab11afa9ce58b179d47c07
Rupal Das <60324778+rupal11@users.noreply.github.com>
2020-10-03 21:12:59 +0530

    Update FizzBuzzUsingTernaryOp.cpp

--- C++/FizzBuzzUsingTernaryOp.cpp
+++ C++/FizzBuzzUsingTernaryOp.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 int main()
 {
-	int i;
-	for(i=1;i<=100;++i)
+	for(int i=1;i<=100;++i)
 		(i%15==0) ? cout<<"FizzBuzz " : (i%3==0) ? cout<<"Fizz " : (i%5==0) ? cout<<"Buzz " : cout<<i<<" ";
 	return 0;
 }
