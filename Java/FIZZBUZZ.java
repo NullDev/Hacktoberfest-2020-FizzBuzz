772c2aaa9ccdc2a1ac7a28a773aa214bf50cf170
Shaurya026 <43774789+Shaurya026@users.noreply.github.com>
2020-10-03 17:19:56 +0530

    FizzBuzz basic implementation using JAVA

---
+++ Java/FIZZBUZZ.java
@@ -0,0 +1,16 @@
+class FIZZBUZZ {
+    public static void main(String[] args){
+        for(int i=1 ; i<100 ; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0){
+                System.out.println("Buzz");
+            }
+        }
+    }
+}
