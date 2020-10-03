69015f6882c8acfc015c301fc37ca1323ba9418f
arpita2712 <35414442+arpita2712@users.noreply.github.com>
2020-10-03 23:39:21 +0530

    Add files via upload
    A simple loop java code for fizz-buzz.

--- Java/FizzBuzz.java
+++ Java/FizzBuzz.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter number:");
-		int n = s.nextInt();
-		IntStream.rangeClosed(1, n)
-	    .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz " : "Fizz ") : (i % 5 == 0 ? "Buzz " : i+" "))
-	    .forEach(System.out::print);
-		s.close();
-	}
+// A Simple loop Java Code for FizzBuzz
+// Author: @arpita2712
+
+import java.io.IOException;
+public class FizzBuzz {
+    public static void main  (String args[]) throws IOException{
+        for(int i=1; i<=100; i++){
+            if(i%3 == 0  && i%5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%5 == 0){
+                System.out.println("Buzz");
+            }
+            else if (i%3 == 0) {
+                System.out.println("Fizz");
+            } 
+            else {
+                System.out.println(i);
+            }
+            
+        }
+    }  
 }
- 
