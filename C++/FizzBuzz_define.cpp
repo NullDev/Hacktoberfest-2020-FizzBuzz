88375413599946707dadbf5b57807d319a0228a2
Alve Svarén <alve@hotmail.se>
2020-10-03 23:34:37 +0200

    Created FizzBuzz_define.cpp

---
+++ C++/FizzBuzz_define.cpp
@@ -0,0 +1,55 @@
+// Author: @alvesvaren
+
+#include <iostream>
+#include <string>
+#define v "Fizz"
+#define w "Buzz"
+#define I int
+#define M main()
+#define O {
+#define C }
+#define f fizz
+#define b buzz
+#define s string
+#define l output
+#define p ()
+#define c ,
+#define P ++
+#define k +=
+#define e =
+#define S ;
+#define o = 0
+#define U using namespace std
+#define L for(I i = 1; i < 101; i P)
+#define F if (f == 3) O
+#define B if (b == 5) O
+#define E if (l.length() <= 0) O
+#define t to_string(i)
+#define G cout <<
+#define N << endl
+
+U S
+
+I M O
+    I f o S
+    I b o S
+    L O
+        s l e s p S
+
+        f P S
+        b P S
+        F
+            f o S
+            l k v S
+        C
+        B
+            b o S
+            l k w S
+        C
+        E
+            l k t S
+        C
+
+        G l N S
+    C
+C
