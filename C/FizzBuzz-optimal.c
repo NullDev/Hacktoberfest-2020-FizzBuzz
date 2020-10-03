876b59f4481162528e480971c1ced53b8a0b3ad3
StephenTrainor <69483754+StephenTrainor@users.noreply.github.com>
2020-10-02 22:47:38 -0700

    Update FizzBuzz-optimal.c
    Added discription

--- C/FizzBuzz-optimal.c
+++ C/FizzBuzz-optimal.c
@@ -1,3 +1,8 @@
+// Not the most concise or memory efficient Fizz Buzz variant, but is easy to add more conditions such as (i % 4 == 0)
+// FizzBuzz usually only prints out the first 100 numbers, but if you want more or less you have the option to.
+// This program has a runtime of O(n) or linear because the time to run increases with the number of iterations linearly
+// Author: @StephenTrainor
+
 #include <stdio.h>
 
 int main(void) {
