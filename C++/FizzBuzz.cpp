b777279b6de291103dfcf88fa880bcae8174769a
Rupal Das <60324778+rupal11@users.noreply.github.com>
2020-10-03 20:49:19 +0530

    Add files via upload

--- C++/FizzBuzz.cpp
+++ C++/FizzBuzz.cpp
@@ -1,21 +1,15 @@
-// FizzBuzz Problem using C++
-// Author: @gourab337
+// A one-liner program to solve FizzBuzz
+// Using ternary operator
+//Author: @rupal11
 
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
-int main(){
-
- for (int i=1; i<101; i++){
-     if(i%3 == 0)
-         cout<<"FIZZ ";
-     if(i%5 == 0)
-      cout<<"BUZZ ";
-     if((i%3)*(i%5))    // if (i%3) && (i%5) !=0, then only the if case gets executed
-        cout<<i<<" ";
-     
-     cout<<",";
-}
-
+int main()
+{
+	int i;
+	for(i=1;i<=100;++i)
+		(i%15==0) ? cout<<"FizzBuzz " : (i%3==0) ? cout<<"Fizz " : (i%5==0) ? cout<<"Buzz " : cout<<i<<" ";
+	return 0;
 }
