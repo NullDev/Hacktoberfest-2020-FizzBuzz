55f00a121949c2a1fce5d45d1e172773f9951a04
Clement <clementjoe99@gmail.com>
2020-10-03 12:10:21 +0530

    added fizzbuzz while solution with strings in dart

---
+++ Dart/fizzbuzz_while_string.dart
@@ -0,0 +1,28 @@
+// FizzBuzz with while loop and printing strings of fizz,buzz and fizzbuzz
+// Author: @acaa06
+
+void main() {
+  var output = '';
+  var fizz='fizz',buzz='buzz',fizzbuzz='fizzbuzz';
+  int i=1;
+  while(i<=100) {
+    
+      if((i % 3 == 0) && (i % 5 == 0)){
+      
+      print(fizzbuzz);
+      }
+     else {
+       if ((i % 3 == 0) || (i % 5 == 0)) {
+      if (i % 3 == 0) {
+        print(fizz);
+      }
+      if (i % 5 == 0) {
+        print(buzz);
+      }
+    }
+    else{
+print(i);
+    }}
+    i++;
+  }
+}
