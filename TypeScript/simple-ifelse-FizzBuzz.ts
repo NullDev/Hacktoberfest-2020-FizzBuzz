ee7da53a99267538e30d9cf70ac0b588c154ab72
Vivek Tiwari <35337366+arimanyus@users.noreply.github.com>
2020-10-03 10:42:04 +0530

    Added a simple 'if-else' TypeScript solution

---
+++ TypeScript/simple-ifelse-FizzBuzz.ts
@@ -0,0 +1,15 @@
+//A simple TypeScript FizzBuzz solution using if-else
+//Author : @arimanyus
+
+for (var i=1; i<101; i++) {
+if (i%15 == 0) {
+    console.log('FizzBuzz');
+}
+else if (i%3 == 0) {
+    console.log('Buzz');
+}
+else if (i%5 == 0) {
+    console.log('Fizz');
+}
+else console.log(i);
+}
