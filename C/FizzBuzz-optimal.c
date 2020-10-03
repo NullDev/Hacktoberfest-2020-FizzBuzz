a435c2ffb6c8dfc107330967f5e5d0f3679f137e
StephenTrainor <69483754+StephenTrainor@users.noreply.github.com>
2020-10-02 22:36:51 -0700

    Create FizzBuzz-optimal.c
    Tried to make a FizzBuzz that was relatively short and also easy/optimal to add more conditions or cases such as (I % 4 == 0).

---
+++ C/FizzBuzz-optimal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int iterations;
+    printf("Enter number to go up to: ");
+    scanf("%d", &iterations);
+    for (int i = 1; i < iterations; i++) {
+        if (i % 3 == 0) {
+            printf("Fizz");
+        }
+        if (i % 5 == 0) {
+            printf("Buzz");
+        }
+        if ((i % 5 != 0) && (i % 3 != 0)) {
+            printf("%i", i);
+        }
+        printf("\n");
+    }
+}
