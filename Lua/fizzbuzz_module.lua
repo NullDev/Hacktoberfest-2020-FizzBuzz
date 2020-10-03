d10acc7f00ef3faf232813b14ede5cb25a31c33d
Darren Kearney <darren@mindcauldron.com>
2020-10-03 15:29:17 +0100

    Added Lua module - easily add fizzbuzz everywhere!

---
+++ Lua/fizzbuzz_module.lua
@@ -0,0 +1,22 @@
+-- A fizzbuzz module. Usage: `fb = require 'fizzbuzz4'; fb:run()` optional counter limits :run(x,y) 
+-- Author: @darrencearnaigh
+
+local fizzbuzz = {}
+
+function fizzbuzz.exec(x)
+   local x, s = tonumber(x), ''
+   if (x % 3 == 0) then s = s..'Fizz' end
+   if (x % 5 == 0) then s = s..'Buzz' end
+   if s == '' then s = x end
+   return s
+end
+
+function fizzbuzz.run(self, x1, x2)
+   local x1 = tonumber(x1) or 1
+   local x2 = tonumber(x2) or 100
+   for i=x1, x2, 1 do
+      print( self.exec(i) )
+   end
+end
+
+return fizzbuzz
