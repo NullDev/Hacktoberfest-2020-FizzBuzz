b79ab56e54f45d7340b089f0b73e6e78f4f9253d
Bennett <hello@vantezzen.io>
2020-10-03 11:59:24 +0200

    Create 74chars.js

---
+++ JavaScript/74chars.js
@@ -0,0 +1,3 @@
+// My attempt in making FizzBuzz in JavaScript with as few characters as possible. I managed to get it down to 74 characters
+// Author: @vantezzen
+for(var b=1;b<101;b++)console.log((b%3<1?'Fizz':'')+(b%5<1?'Buzz':'')||b);
