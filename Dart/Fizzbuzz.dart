a90b34695fc007b61fb11e74e90fa21064a83325
ajtheb <apoorvjain.2122@gmail.com>
2020-10-03 22:39:17 +0530

    update

--- Dart/Fizzbuzz.dart
+++ Dart/Fizzbuzz.dart
@@ -1,21 +1,12 @@
-//Fizzbuzz program in dart
-//author yasharth291
-import 'dart:io';
 void main() {
-  int i = 1;
-  while (i <= 100) {
-    if ((i % 3 == 0) || (i % 5 == 0)) {
-      if (i % 3 == 0) {
-        stdout.write("Fizz");
-      }
-      if (i % 5 == 0) {
-        print("Buzz");
-      } else {
-        print("");
-      }
-    } else {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 15 == 0)
+      print("FizzBuzz");
+    else if (i % 3 == 0)
+      print("Fizz");
+    else if (i % 5 == 0)
+      print("Buzz");
+    else
       print(i);
-    }
-    i++;
   }
 }
