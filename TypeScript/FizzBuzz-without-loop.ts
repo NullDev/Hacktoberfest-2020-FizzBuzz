757ee9f23c902eff4d9aa9eb03928f94e0169d54
Nicolas Ehrenfeld <nicolas@meetdeal.fr>
2020-10-04 04:45:14 +0200

    Adding FizzBuzz without loop in TS language

---
+++ TypeScript/FizzBuzz-without-loop.ts
@@ -0,0 +1,24 @@
+// FizzBuzz without loop, only timeout very 100ms
+// Author: @EhrenfeldN
+
+class FizzBuzz {
+
+    public index: number;
+    public intervale: number;
+
+    constructor() {
+        this.intervale = setInterval( () => {
+            if (this.index % 3 === 0 && this.index % 5 === 0) this.printResult("FizzBuzz");
+            else if (this.index % 3 === 0) this.printResult("Fizz");
+            else if (this.index % 5 === 0) this.printResult("Buzz");
+            else this.printResult(this.index);
+        }, 100);
+    }
+
+    public printResult(text_to_display: string | number): void {
+        console.log(text_to_display);
+        this.index++;
+        if (this.index > 100) clearInterval(this.intervale);
+    }
+
+}
