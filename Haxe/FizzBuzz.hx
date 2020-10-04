8075789dcb44dcfe90e87447cf3a3b788e575d88
Tuatarian <betterthangeowizard@gmail.com>
2020-10-03 17:07:55 -0700

    Haxe FizzBuzz

---
+++ Haxe/FizzBuzz.hx
@@ -0,0 +1,15 @@
+class FizzBuzz {
+    static function main() {
+        for (i in (1...100)) {
+            if (i % 15 == 0) {
+                trace("FizzBuzz");
+            } else if (i % 3 == 0) {
+                trace("Fizz");
+            } else if (i % 5 == 0) {
+                trace("Buzz");
+            } else {
+                trace(i);
+            }
+        }
+    }
+}
