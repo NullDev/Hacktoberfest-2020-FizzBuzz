84b36a790ae26c0f91e09ec03a975636628a3d0f
mzelenetz <mzelenetz@g.harvard.edu>
2020-10-05 00:13:24 -0400

    fixing errors, cleaning code, making more similar to Joel's

--- Python/MLFizzBuzz.py
+++ Python/MLFizzBuzz.py
@@ -1,102 +1,50 @@
-# StupidFizzBuzz
 # Author: @mzelenetz
+"""
+The motivation for this solution was based on Joel Grus' Fizz Buzz in Tensorflow Blog Post: https://joelgrus.com/2016/05/23/fizz-buzz-in-tensorflow/
+I wanted to see if I could do something similar with random forests.
+
+We are going to train on 101-2048 then we are going to predict on fizzbuzz from numbers 1-100 
+"""
+
 import numpy as np 
-import pandas as pd 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
 
-SERIES_LENGTH = 10
-
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+# Encodings from Joel Grus' blog
+def binary_encode(i, num_digits):
     """
-    Frame a time series as a supervised learning dataset.
-    Arguments:
-    ^Idata: Sequence of observations as a list or NumPy array.
-    ^In_in: Number of lag observations as input (X).
-    ^In_out: Number of observations as output (y).
-    ^Idropnan: Boolean whether or not to drop rows with NaN values.
-    Returns:
-    ^IPandas DataFrame of series framed for supervised learning.
+    Encode the features
     """
-    n_vars = 1 if type(data) is list else data.shape[1]
-    df = pd.DataFrame(data)
-    cols, names = list(), list()
-    # input sequence (t-n, ... t-1)
-    for i in range(n_in, 0, -1):
-        cols.append(df.shift(i))
-        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-    # forecast sequence (t, t+1, ... t+n)
-    for i in range(0, n_out):
-        cols.append(df.shift(-i))
-        if i == 0:
-            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-        else:
-            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-    # put it all together
-    agg = pd.concat(cols, axis=1)
-    agg.columns = names
-    # drop rows with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
-    return agg
+    return np.array([i >> d & 1 for d in range(num_digits)])
 
-def fizzbuzz(num):
-    if num % 15 == 0:
-        return 'FizzBuzz'
-    elif num % 5 == 0:
-        return 'Buzz'
-    elif num % 3 == 0:
-        return 'Fizz'
-    else:
-        return '<number>'
-
-def create_input_features(inputs):
+def fizz_buzz_encode(i):
     """
-    [labels, [features]]
+    Encode the labels either Fizzbuzz, Fizz, Buzz, or the number
     """
-    pad = ['TOKEN'] * SERIES_LENGTH
-    for index in range(len(inputs) - SERIES_LENGTH - 1):
-        yield [inputs[index], pad + inputs[max(0, index - SERIES_LENGTH ):index]]
-        pad = pad[1:]
-    
+    if   i % 15 == 0: return np.array([0, 0, 0, 1]) #fizzbuzz
+    elif i % 5  == 0: return np.array([0, 0, 1, 0]) #buzz
+    elif i % 3  == 0: return np.array([0, 1, 0, 0]) #fizz
+    else:             return np.array([1, 0, 0, 0]) #the number
 
-training = [fizzbuzz(num) for num in range(1, 1000)]
-training_inputs = list(create_input_features(training))
-
-lb = LabelBinarizer()
-lb.fit( training + ['TOKEN'] )
-
-
-X = np.array([lb.transform(features).flatten() for label, features in training_inputs])
-y = np.array([label for label, features in training_inputs])
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=6251)
+def fizz_buzz(i, prediction):
+    """
+    Unpack the predictions
+    """
+    return [str(i), "fizz", "buzz", "fizzbuzz"][prediction]
 
 
-#clf = LogisticRegression(tol=1e-6)
+NUM_DIGITS = 10
+X_train = np.array([binary_encode(i, NUM_DIGITS) for i in range(101, 2 ** NUM_DIGITS)])
+y_train = np.array([fizz_buzz_encode(i)          for i in range(101, 2 ** NUM_DIGITS)])
+
+X_test = np.array([binary_encode(i, NUM_DIGITS) for i in range(1, 100)])
+y_test = np.array([fizz_buzz_encode(i)          for i in range(1, 100)])
+
+# Train the Random Forest
 regr = RandomForestClassifier(max_depth=20, random_state=6251)
 regr.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
 predicted = regr.predict(X_test)
-accuracy_score(y_test, predicted)
 
-test = list(create_input_features([num for num in range(1000, 1100)]))
+for i in range(1, 100):
+    print(fizz_buzz(i, y_test[i-1].tolist().index(1)))
 
-res = []
-pad = ['PAD'] * SERIES_LENGTH
-input = []
-res = []
-for i in range(1000, 1101):
-    X = lb.transform(pad + input)
-    predicted = regr.predict([np.array(X).flatten()])[0]
-    if predicted == "<number>":
-        res.append(str(i))
-    else:
-        res.append(predicted)
-    input.append(predicted)
-    pad = pad[1:]
-    if len(input) > SERIES_LENGTH:
-        input = input[1:]
