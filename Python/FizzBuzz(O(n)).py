ad816c2db620b56afb1301872d7319e40b05b058
NullDev (Chris) <shadow@nulldev.org>
2020-10-05 16:08:24 +0200

    Delete FizzBuzz(O(n)).py

--- Python/FizzBuzz(O(n)).py
+++
@@ -1,9 +0,0 @@
-for i in range(1,101):
-    if(i%3==0 and i%5==0):
-        print("FizzBuzz")
-    if(i%3==0):
-        print("Fizz")
-    if(i%5==0):
-        print("Buzz")
-    else:
-        print(i)
