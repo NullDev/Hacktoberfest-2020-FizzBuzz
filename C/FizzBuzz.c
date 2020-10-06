5230cdf711ea07e447e0e87aab3059417a782052
NullDev (Chris) <shadow@nulldev.org>
2020-10-06 21:36:55 +0200

    Update FizzBuzz.c

--- C/FizzBuzz.c
+++ C/FizzBuzz.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 int main(void)
 {
-    int i = 1;
-    for (; i <= 100; ++i)
+    int i;
+    for (i=1; i <= 100; ++i)
     {
         printf("Number: %d %s%s\n", i, i % 3 ? "" : "Fizz", i % 5 ? "" : "Buzz");
     }
