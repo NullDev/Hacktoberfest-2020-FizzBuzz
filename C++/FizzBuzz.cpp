92135d62aa512dec7d8072fb0a3935d1cfa8d329
NullDev (Chris) <shadow@nulldev.org>
2020-10-06 21:21:53 +0200

    Re-added file

---
+++ C++/FizzBuzz.cpp
@@ -0,0 +1,21 @@
+// FizzBuzz Problem using C++
+// Author: @gourab337
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+ for (int i=1; i<101; i++){
+     if(i%3 == 0)
+         cout<<"FIZZ ";
+     if(i%5 == 0)
+      cout<<"BUZZ ";
+     if((i%3)*(i%5))    // if (i%3) && (i%5) !=0, then only the if case gets executed
+        cout<<i<<" ";
+
+     cout<<",";
+}
+
+}  
